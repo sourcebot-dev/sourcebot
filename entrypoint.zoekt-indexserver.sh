ac3e8313b3f04c283c76fe0748ffc0ab3d792d4a
bkellam <bshizzle1234@gmail.com>
2024-09-04 13:55:47 -0700

    Add gitlab support

--- entrypoint.zoekt-indexserver.sh
+++ entrypoint.zoekt-indexserver.sh
@@ -13,4 +13,12 @@ else
     echo -e "\e[33mWarning: Private GitHub repositories will not be indexed since GITHUB_TOKEN was not set. If you are not using GitHub, disregard.\e[0m"
 fi
 
+# Check if GITLAB_TOKEN is set
+if [ -n "$GITLAB_TOKEN" ]; then
+    echo "$GITLAB_TOKEN" > "$HOME/.gitlab-token"
+    chmod 600 "$HOME/.gitlab-token"
+else
+    echo -e "\e[33mWarning: GitLab repositories will not be indexed since GITLAB_TOKEN was not set. If you are not using GitLab, disregard.\e[0m"
+fi
+
 exec "zoekt-indexserver" "-data_dir" "${ZOEKT_DATA_CACHE_DIR}" "-mirror_config" "${CONFIG_PATH}"
