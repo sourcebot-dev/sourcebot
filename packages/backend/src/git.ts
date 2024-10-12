7346e59399fd3058486bba9aff48af658521fa88
bkellam <bshizzle1234@gmail.com>
2024-10-12 14:50:08 -0700

    Add progress callback for cloning

--- packages/backend/src/git.ts
+++ packages/backend/src/git.ts
@@ -5,14 +5,26 @@ import { createLogger } from './logger.js';
 
 const logger = createLogger('git');
 
-export const cloneRepository = async (repo: Repository) => {
+export const cloneRepository = async (repo: Repository, onProgress?: (progress: number) => void) => {
     if (existsSync(repo.path)) {
         logger.warn(`${repo.id} already exists. Skipping clone.`)
         return;
     }
 
-    const progress = ({ method, stage, progress }: SimpleGitProgressEvent) => {
-        console.log(`git.${method} ${stage} stage ${progress}% complete.`);
+    let receivingProgress = 0;
+    let resolvingProgress = 0;
+
+    const progress = ({ stage, processed, total }: SimpleGitProgressEvent) => {
+        // Guestimate the progress as the average of receiving and resolving stages.
+        if (stage === 'receiving') {
+            receivingProgress = processed / total;
+        }
+        if  (stage === 'resolving') {
+            resolvingProgress = processed / total;
+        }
+
+        const totalProgress = (receivingProgress + resolvingProgress) / 2;
+        onProgress?.(totalProgress);
     }
 
     const git = simpleGit({
