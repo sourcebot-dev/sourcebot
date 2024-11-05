d7124ce94193fe667f04d682829b921a638de531
bkellam <bshizzle1234@gmail.com>
2024-11-05 15:01:46 -0800

    schema docs

--- packages/backend/src/schemas/v2.ts
+++ packages/backend/src/schemas/v2.ts
@@ -60,8 +60,17 @@ export interface GitHubConfig {
   };
   revisions?: GitRevisions;
 }
+/**
+ * The revisions (branches, tags) that should be included when indexing. The default branch (HEAD) is always indexed.
+ */
 export interface GitRevisions {
+  /**
+   * List of branches to include when indexing. For a given repo, only the branches that exist on the repo's remote *and* match at least one of the provided `branches` will be indexed. The default branch (HEAD) is always indexed. Glob patterns are supported.
+   */
   branches?: string[];
+  /**
+   * List of tags to include when indexing. For a given repo, only the tags that exist on the repo's remote *and* match at least one of the provided `tags` will be indexed. Glob patterns are supported.
+   */
   tags?: string[];
 }
 export interface GitLabConfig {
