4e927654a49c70fd893c96828712310ee4078abb
bkellam <bshizzle1234@gmail.com>
2024-11-06 17:56:15 -0800

    hide scrollbars

--- packages/web/tailwind.config.ts
+++ packages/web/tailwind.config.ts
@@ -74,7 +74,10 @@ const config = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('tailwind-scrollbar-hide')
+  ],
 } satisfies Config
 
 export default config
