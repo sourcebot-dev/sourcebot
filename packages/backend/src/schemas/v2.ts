4eadccfbe56272d02c27f902adcbe1e3075e5573
bkellam <bshizzle1234@gmail.com>
2024-11-14 22:23:57 -0800

    enable allow_missing_branches

--- packages/backend/src/schemas/v2.ts
+++ packages/backend/src/schemas/v2.ts
@@ -54,7 +54,7 @@ export interface GitHubConfig {
      */
     archived?: boolean;
     /**
-     * List of individual repositories to exclude from syncing. Expected to be formatted as '{orgName}/{repoName}' or '{userName}/{repoName}'.
+     * List of individual repositories to exclude from syncing. Glob patterns are supported.
      */
     repos?: string[];
   };
@@ -115,7 +115,7 @@ export interface GitLabConfig {
      */
     archived?: boolean;
     /**
-     * List of individual projects to exclude from syncing. The project's namespace must be specified. See: https://docs.gitlab.com/ee/user/namespace/
+     * List of projects to exclude from syncing. Glob patterns are supported. The project's namespace must be specified, see: https://docs.gitlab.com/ee/user/namespace/
      */
     projects?: string[];
   };
@@ -163,7 +163,7 @@ export interface GiteaConfig {
      */
     archived?: boolean;
     /**
-     * List of individual repositories to exclude from syncing. Expected to be formatted as '{orgName}/{repoName}' or '{userName}/{repoName}'.
+     * List of individual repositories to exclude from syncing. Glob patterns are supported.
      */
     repos?: string[];
   };
