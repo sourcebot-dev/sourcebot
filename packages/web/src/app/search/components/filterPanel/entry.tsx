913cb52b8bc6a96ee91beba453d0be415f0a2e9a
Konrad Staniszewski <konrad.a.staniszewski@gmail.com>
2024-12-02 15:38:31 -0800

    Fix filter items text
    when the filter items were cutoff, the text would break onto multiple
    lines and the count would overlap with the text

--- packages/web/src/app/search/components/filterPanel/entry.tsx
+++ packages/web/src/app/search/components/filterPanel/entry.tsx
@@ -25,10 +25,14 @@ export const Entry = ({
     },
     onClicked,
 }: EntryProps) => {
-
+    let countText = count.toString();
+    if (count > 999) {
+        countText = "999+";
+    }
     return (
         <div
-            className={clsx("flex flex-row items-center justify-between py-0.5 px-2 cursor-pointer rounded-md gap-2 select-none",
+            className={clsx(
+                "flex flex-row items-center justify-between py-0.5 px-2 cursor-pointer rounded-md gap-2 select-none",
                 {
                     "hover:bg-gray-200 dark:hover:bg-gray-700": !isSelected,
                     "bg-blue-200 dark:bg-blue-400": isSelected,
@@ -36,13 +40,15 @@ export const Entry = ({
             )}
             onClick={() => onClicked()}
         >
-            <div className="flex flex-row items-center gap-1">
+            <div className="flex flex-row items-center gap-1 overflow-hidden">
                 {Icon ? Icon : (
                     <QuestionMarkCircledIcon className="w-4 h-4 flex-shrink-0" />
                 )}
-                <p className="text-wrap">{displayName}</p>
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap">{displayName}</p>
             </div>
-            <p>{count}</p>
+            <div className="px-2 py-0.5 bg-gray-100 dark:bg-gray-800 text-sm rounded-md">
+                {countText}
+            </div>
         </div>
     );
 }
