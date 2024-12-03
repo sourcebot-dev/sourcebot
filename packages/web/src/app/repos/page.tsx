0979b074cd20dd92bf68b7ae7bf7c75a56d3889b
Konrad Staniszewski <konrad.a.staniszewski@gmail.com>
2024-12-02 21:51:22 -0800

    make number of table elements dynamic

--- packages/web/src/app/repos/page.tsx
+++ packages/web/src/app/repos/page.tsx
@@ -7,7 +7,7 @@ export default function ReposPage() {
         <div className="h-screen flex flex-col items-center">
             <NavigationMenu />
             <Suspense fallback={<div>Loading...</div>}>
-                <div className="max-w-[90%]">
+                <div className="h-fill max-w-[90%]">
                     <RepositoryTable />
                 </div>
             </Suspense>
