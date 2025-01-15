989430ee0280f7bf6a835c1b8643fdbc81b3b85a
bkellam <bshizzle1234@gmail.com>
2025-01-15 14:32:05 -0800

    Add prisma db adapter. Add basic login screen

--- packages/web/src/auth.ts
+++ packages/web/src/auth.ts
@@ -1,8 +1,38 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { prisma } from "@/prisma";
+import type { Provider } from "next-auth/providers"
+
+const providers: Provider[] = [
+    GitHub,
+];
+
+// @see: https://authjs.dev/guides/pages/signin
+export const providerMap = providers
+    .map((provider) => {
+        if (typeof provider === "function") {
+            const providerData = provider()
+            return { id: providerData.id, name: providerData.name }
+        } else {
+            return { id: provider.id, name: provider.name }
+        }
+    })
+    .filter((provider) => provider.id !== "credentials");
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [
-        GitHub
-    ],
+    adapter: PrismaAdapter(prisma),
+    session: {
+        strategy: "jwt",
+    },
+    providers: providers,
+    callbacks: {
+        authorized: async ({ auth }) => {
+            return !!auth
+        },
+    },
+    pages: {
+        signIn: "/login"
+    }
 })
--- packages/web/src/middleware.ts
+++ packages/web/src/middleware.ts
@@ -1,1 +1,9 @@
-export { auth as middleware } from "@/auth";
+
+export {
+    auth as middleware
+} from "@/auth";
+
+export const config = {
+    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico|sitemap.xml|robots.txt).*)'],
+}
