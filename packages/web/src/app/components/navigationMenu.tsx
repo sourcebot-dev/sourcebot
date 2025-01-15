00364d374e6f9389e705bd48c8053f63d3334e82
bkellam <bshizzle1234@gmail.com>
2025-01-15 09:17:09 -0800

    Initial wip using auth.js

--- packages/web/src/app/components/navigationMenu.tsx
+++ packages/web/src/app/components/navigationMenu.tsx
@@ -1,90 +1,80 @@
-'use client';
-
 import { Button } from "@/components/ui/button";
 import { NavigationMenu as NavigationMenuBase, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
 import Link from "next/link";
-import { GitHubLogoIcon, DiscordLogoIcon } from "@radix-ui/react-icons";
-import { SettingsDropdown } from "./settingsDropdown";
 import { Separator } from "@/components/ui/separator";
 import Image from "next/image";
 import logoDark from "../../../public/sb_logo_dark_small.png";
 import logoLight from "../../../public/sb_logo_light_small.png";
-import { useRouter } from "next/navigation";
+import { ProfilePicture } from "./profilePicture";
+import { auth, signIn } from "@/auth";
 
-const SOURCEBOT_DISCORD_URL = "https://discord.gg/6Fhp27x7Pb";
-const SOURCEBOT_GITHUB_URL = "https://github.com/sourcebot-dev/sourcebot";
-
-export const NavigationMenu = () => {
-    const router = useRouter();
+export const NavigationMenu = async () => {
+    const session = await auth();
 
     return (
         <div className="flex flex-col w-screen h-fit">
-                <div className="flex flex-row justify-between items-center py-1.5 px-3">
-                    <div className="flex flex-row items-center">
-                        <div
-                            className="mr-3 cursor-pointer"
-                            onClick={() => {
-                                router.push("/");
-                            }}
-                        >
-                            <Image
-                                src={logoDark}
-                                className="h-11 w-auto hidden dark:block"
-                                alt={"Sourcebot logo"}
-                                priority={true}
-                            />
-                            <Image
-                                src={logoLight}
-                                className="h-11 w-auto block dark:hidden"
-                                alt={"Sourcebot logo"}
-                                priority={true}
-                            />
-                        </div>
+            <div className="flex flex-row justify-between items-center py-1.5 px-3">
+                <div className="flex flex-row items-center">
+                    <Link
+                        href="/"
+                        className="mr-3 cursor-pointer"
+                    >
+                        <Image
+                            src={logoDark}
+                            className="h-11 w-auto hidden dark:block"
+                            alt={"Sourcebot logo"}
+                            priority={true}
+                        />
+                        <Image
+                            src={logoLight}
+                            className="h-11 w-auto block dark:hidden"
+                            alt={"Sourcebot logo"}
+                            priority={true}
+                        />
+                    </Link>
 
-                        <NavigationMenuBase>
-                            <NavigationMenuList>
-                                <NavigationMenuItem>
-                                    <Link href="/" legacyBehavior passHref>
-                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                            Search
-                                        </NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                    <Link href="/repos" legacyBehavior passHref>
-                                        <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                            Repositories
-                                        </NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                            </NavigationMenuList>
-                        </NavigationMenuBase>
-                    </div>
-
-                    <div className="flex flex-row items-center gap-2">
-                        <Button
-                            variant="outline"
-                            size="icon"
-                            onClick={() => {
-                                window.open(SOURCEBOT_DISCORD_URL, "_blank");
-                            }}
-                        >
-                            <DiscordLogoIcon className="w-4 h-4" />
-                        </Button>
-                        <Button
-                            variant="outline"
-                            size="icon"
-                            onClick={() => {
-                                window.open(SOURCEBOT_GITHUB_URL, "_blank");
-                            }}
-                        >
-                            <GitHubLogoIcon className="w-4 h-4" />
-                        </Button>
-                        <SettingsDropdown />
-                    </div>
+                    <NavigationMenuBase>
+                        <NavigationMenuList>
+                            <NavigationMenuItem>
+                                <Link href="/" legacyBehavior passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Search
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                            <NavigationMenuItem>
+                                <Link href="/repos" legacyBehavior passHref>
+                                    <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                                        Repositories
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        </NavigationMenuList>
+                    </NavigationMenuBase>
                 </div>
-                <Separator />
+
+                <div className="flex flex-row items-center gap-2">
+                    {!session ? (
+                        <form
+                            action={async () => {
+                                "use server";
+                                await signIn("github");
+                            }}
+                        >
+                            <Button
+                                variant="outline"
+                                size="default"
+                            >
+                                Login
+                            </Button>
+                        </form>
+                    ) : (
+                        <ProfilePicture />
+                    )}
+                </div>
             </div>
+            <Separator />
+        </div>
 
 
     )
