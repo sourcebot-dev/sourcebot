2111a77a5c95cf65b2531f218d5128eae5cc33ae
bkellam <bshizzle1234@gmail.com>
2025-02-15 10:03:13 -0800

    Fix build with suspense boundary

--- packages/web/src/app/login/page.tsx
+++ packages/web/src/app/login/page.tsx
@@ -1,9 +1,12 @@
 import { LoginForm } from "./components/loginForm";
+import { Suspense } from "react";
 
 export default async function Login() {
     return (
         <div className="flex flex-col justify-center items-center h-screen">
-            <LoginForm />
+            <Suspense fallback={<div>Loading...</div>}>
+                <LoginForm />
+            </Suspense>
         </div>
     )
 }
