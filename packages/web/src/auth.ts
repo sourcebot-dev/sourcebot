86a80a4f73bc34846bf5d031e0adc57fa2528f41
Brendan Kellam <bshizzle1234@gmail.com>
2025-02-14 21:00:45 -0800

    Credentials provider (#192)
    * email password functionality
    
    * feedback

--- packages/web/src/auth.ts
+++ packages/web/src/auth.ts
@@ -2,12 +2,16 @@ import 'next-auth/jwt';
 import NextAuth, { DefaultSession } from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
 import { PrismaAdapter } from "@auth/prisma-adapter"
 import { prisma } from "@/prisma";
 import { AUTH_GITHUB_CLIENT_ID, AUTH_GITHUB_CLIENT_SECRET, AUTH_GOOGLE_CLIENT_ID, AUTH_GOOGLE_CLIENT_SECRET, AUTH_SECRET, AUTH_URL } from "./lib/environment";
 import { User } from '@sourcebot/db';
 import 'next-auth/jwt';
 import type { Provider } from "next-auth/providers";
+import { verifyCredentialsRequestSchema, verifyCredentialsResponseSchema } from './lib/schemas';
+
+export const runtime = 'nodejs';
 
 declare module 'next-auth' {
     interface Session {
@@ -32,6 +36,39 @@ const providers: Provider[] = [
         clientId: AUTH_GOOGLE_CLIENT_ID,
         clientSecret: AUTH_GOOGLE_CLIENT_SECRET,
     }),
+    Credentials({
+        credentials: {
+            email: {},
+            password: {}
+        },
+        type: "credentials",
+        authorize: async (credentials) => {
+            const body = verifyCredentialsRequestSchema.safeParse(credentials);
+            if (!body.success) {
+                return null;
+            }
+            const { email, password } = body.data;
+
+            // authorize runs in the edge runtime (where we cannot make DB calls / access environment variables),
+            // so we need to make a request to the server to verify the credentials.
+            const response = await fetch(new URL('/api/auth/verifyCredentials', AUTH_URL), {
+                method: 'POST',
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (!response.ok) {
+                return null;
+            }
+
+            const user = verifyCredentialsResponseSchema.parse(await response.json());
+            return {
+                id: user.id,
+                email: user.email,
+                name: user.name,
+                image: user.image,
+            }
+        }
+    })
 ];
 
 // @see: https://authjs.dev/guides/pages/signin
