7af729a553a2c083c24bcb0aa59279cb1a1f4a12
Erwan Le Gall <erwan.le-gall@kosmos.fr>
2025-02-17 15:13:01 +0100

    Set resync and reindex period as environment variable

--- packages/backend/src/constants.ts
+++ packages/backend/src/constants.ts
@@ -1,16 +1,6 @@
 import { Settings } from "./types.js";
 
 /**
- * The interval to reindex a given repository.
- */
-export const REINDEX_INTERVAL_MS = 1000 * 60 * 60;
-
-/**
- * The interval to re-sync the config.
- */
-export const RESYNC_CONFIG_INTERVAL_MS = 1000 * 60 * 60 * 24;
-
-/**
  * Default settings.
  */
 export const DEFAULT_SETTINGS: Settings = {
