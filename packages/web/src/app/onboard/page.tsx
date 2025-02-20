ebed5cdca44774865a18f4367157988c755e5b6b
bkellam <bshizzle1234@gmail.com>
2025-02-19 17:09:45 -0800

    Add logout escape hatch

--- packages/web/src/app/onboard/page.tsx
+++ packages/web/src/app/onboard/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/auth";
 import { redirect } from "next/navigation";
 import { OnboardHeader } from "./components/onboardHeader";
 import { OnboardingSteps } from "@/lib/constants";
+import { LogoutEscapeHatch } from "../components/logoutEscapeHatch";
 
 export default async function Onboarding() {
     const session = await auth();
@@ -11,13 +12,14 @@ export default async function Onboarding() {
     }
 
     return (
-        <div className="flex flex-col items-center min-h-screen p-12 bg-backgroundSecondary fade-in-20">
+        <div className="flex flex-col items-center min-h-screen p-12 bg-backgroundSecondary fade-in-20 relative">
             <OnboardHeader
                 title="Setup your organization"
                 description="Create a organization for your team to search and share code across your repositories."
                 step={OnboardingSteps.CreateOrg}
             />
             <OrgCreateForm />
+            <LogoutEscapeHatch className="absolute top-0 right-0 p-12" />
         </div>
     );
 }
