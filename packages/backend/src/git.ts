386a3b52d7add85a28813715e0c3aa3296bf84fc
Brendan Kellam <bshizzle1234@gmail.com>
2025-02-26 16:55:10 -0800

    remove git & local schemas (#213)

--- packages/backend/src/git.ts
+++ packages/backend/src/git.ts
@@ -1,10 +1,4 @@
-import { GitRepository, AppContext } from './types.js';
 import { simpleGit, SimpleGitProgressEvent } from 'simple-git';
-import { createLogger } from './logger.js';
-import { GitConfig } from "@sourcebot/schemas/v2/index.type"
-import path from 'path';
-
-const logger = createLogger('git');
 
 export const cloneRepository = async (cloneURL: string, path: string, gitConfig?: Record<string, string>, onProgress?: (event: SimpleGitProgressEvent) => void) => {
     const git = simpleGit({
@@ -44,81 +38,4 @@ export const fetchRepository = async (path: string, onProgress?: (event: SimpleG
             "--progress"
         ]
     );
-}
-
-const isValidGitRepo = async (url: string): Promise<boolean> => {
-    const git = simpleGit();
-    try {
-        await git.listRemote([url]);
-        return true;
-    } catch (error) {
-        logger.debug(`Error checking if ${url} is a valid git repo: ${error}`);
-        return false;
-    }
-}
-
-const stripProtocolAndGitSuffix = (url: string): string => {
-    return url.replace(/^[a-zA-Z]+:\/\//, '').replace(/\.git$/, '');
-}
-
-const getRepoNameFromUrl = (url: string): string => {
-    const strippedUrl = stripProtocolAndGitSuffix(url);
-    return strippedUrl.split('/').slice(-2).join('/');
-}
-
-export const getGitRepoFromConfig = async (config: GitConfig, ctx: AppContext) => {
-    const repoValid = await isValidGitRepo(config.url);
-    if (!repoValid) {
-        logger.error(`Git repo provided in config with url ${config.url} is not valid`);
-        return null;
-    }
-
-    const cloneUrl = config.url;
-    const repoId = stripProtocolAndGitSuffix(cloneUrl);
-    const repoName = getRepoNameFromUrl(config.url);
-    const repoPath = path.resolve(path.join(ctx.reposPath, `${repoId}.git`));
-    const repo: GitRepository = {
-        vcs: 'git',
-        id: repoId,
-        name: repoName,
-        path: repoPath,
-        isStale: false,
-        cloneUrl: cloneUrl,
-        branches: [],
-        tags: [],
-    }
-
-    if (config.revisions) {
-        if (config.revisions.branches) {
-            const branchGlobs = config.revisions.branches;
-            const git = simpleGit();
-            const branchList = await git.listRemote(['--heads', cloneUrl]);
-            const branches = branchList
-                .split('\n')
-                .map(line => line.split('\t')[1])
-                .filter(Boolean)
-                .map(branch => branch.replace('refs/heads/', ''));
-
-            repo.branches = branches.filter(branch =>
-                branchGlobs.some(glob => new RegExp(glob).test(branch))
-            );
-        }
-
-        if (config.revisions.tags) {
-            const tagGlobs = config.revisions.tags;
-            const git = simpleGit();
-            const tagList = await git.listRemote(['--tags', cloneUrl]);
-            const tags = tagList
-                .split('\n')
-                .map(line => line.split('\t')[1])
-                .filter(Boolean)
-                .map(tag => tag.replace('refs/tags/', ''));
-
-            repo.tags = tags.filter(tag =>
-                tagGlobs.some(glob => new RegExp(glob).test(tag))
-            );
-        }
-    }
-
-    return repo;
 }
--- packages/backend/src/utils.ts
+++ packages/backend/src/utils.ts
@@ -1,7 +1,6 @@
 import { Logger } from "winston";
-import { AppContext, Repository } from "./types.js";
+import { AppContext } from "./types.js";
 import path from 'path';
-import micromatch from "micromatch";
 import { PrismaClient, Repo } from "@sourcebot/db";
 import { decrypt } from "@sourcebot/crypto";
 import { Token } from "@sourcebot/schemas/v3/shared.type";
@@ -19,74 +18,6 @@ export const measure = async <T>(cb: () => Promise<T>) => {
 
 export const marshalBool = (value?: boolean) => {
     return !!value ? '1' : '0';
-}
-
-export const excludeForkedRepos = <T extends Repository>(repos: T[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        if (!!repo.isFork) {
-            logger?.debug(`Excluding repo ${repo.id}. Reason: \`exclude.forks\` is true`);
-            return false;
-        }
-        return true;
-    });
-}
-
-export const excludeArchivedRepos = <T extends Repository>(repos: T[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        if (!!repo.isArchived) {
-            logger?.debug(`Excluding repo ${repo.id}. Reason: \`exclude.archived\` is true`);
-            return false;
-        }
-        return true;
-    });
-}
-
-
-export const excludeReposByName = <T extends Repository>(repos: T[], excludedRepoNames: string[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        if (micromatch.isMatch(repo.name, excludedRepoNames)) {
-            logger?.debug(`Excluding repo ${repo.id}. Reason: \`exclude.repos\` contains ${repo.name}`);
-            return false;
-        }
-        return true;
-    });
-}
-
-export const includeReposByName = <T extends Repository>(repos: T[], includedRepoNames: string[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        if (micromatch.isMatch(repo.name, includedRepoNames)) {
-            logger?.debug(`Including repo ${repo.id}. Reason: \`repos\` contain ${repo.name}`);
-            return true;
-        }
-        return false;
-    });
-}
-
-export const includeReposByTopic = <T extends Repository>(repos: T[], includedRepoTopics: string[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        const topics = repo.topics ?? [];
-        const matchingTopics = topics.filter((topic) => micromatch.isMatch(topic, includedRepoTopics));
-
-        if (matchingTopics.length > 0) {
-
-            logger?.debug(`Including repo ${repo.id}. Reason: \`topics\` matches the following topics: ${matchingTopics.join(', ')}`);
-            return true;
-        }
-        return false;
-    });
-}
-
-export const excludeReposByTopic = <T extends Repository>(repos: T[], excludedRepoTopics: string[], logger?: Logger) => {
-    return repos.filter((repo) => {
-        const topics = repo.topics ?? [];
-        const matchingTopics = topics.filter((topic) => micromatch.isMatch(topic, excludedRepoTopics));
-
-        if (matchingTopics.length > 0) {
-            logger?.debug(`Excluding repo ${repo.id}. Reason: \`exclude.topics\` matches the following topics: ${matchingTopics.join(', ')}`);
-            return false;
-        }
-        return true;
-    });
 }
 
 export const getTokenFromConfig = async (token: Token, orgId: number, db?: PrismaClient) => {
