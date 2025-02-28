58f171fab79d3dad5a5abc57c494e76a6d4ce6ed
msukkari <michael.sukkarieh@mail.mcgill.ca>
2025-02-28 14:18:32 -0800

    fix styling

--- packages/web/src/app/[domain]/repos/page.tsx
+++ packages/web/src/app/[domain]/repos/page.tsx
@@ -2,18 +2,22 @@ import { NavigationMenu } from "../components/navigationMenu";
 import { RepositoryTable } from "./repositoryTable";
 import { getOrgFromDomain } from "@/data/org";
 import { PageNotFound } from "../components/pageNotFound";
-
+import { Header } from "../components/header";
 export default async function ReposPage({ params: { domain } }: { params: { domain: string } }) {
     const org = await getOrgFromDomain(domain);
     if (!org) {
         return <PageNotFound />
     }
-    
+
     return (
-        <div className="h-screen flex flex-col items-center">
-            <NavigationMenu domain={domain} />
-            <div className="w-[75%]">
-                <RepositoryTable />
+        <div>
+            <Header>
+                <h1 className="text-3xl">Repositories</h1>
+            </Header>
+            <div className="h-screen flex flex-col items-center">
+                <div className="w-full">
+                    <RepositoryTable />
+                </div>
             </div>
         </div>
     )
