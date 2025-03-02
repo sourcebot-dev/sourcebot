1653619fcde107da52dc120d126762328cad2350
msukkari <michael.sukkarieh@mail.mcgill.ca>
2025-03-01 17:21:52 -0800

    set sentry environemnt from env var

--- packages/web/sentry.edge.config.ts
+++ packages/web/sentry.edge.config.ts
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
   dsn: "https://d1102cafbf02e1d3ed04a6de141b1dce@o4508802051932160.ingest.us.sentry.io/4508905421078528",
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT || 'unknown',
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
