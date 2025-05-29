919bac7355a64cfed453130d02a1ab2d3bb070ab
bkellam <bshizzle1234@gmail.com>
2025-05-28 17:28:43 -0700

    fix

--- packages/web/src/features/entitlements/constants.ts
+++ packages/web/src/features/entitlements/constants.ts
@@ -3,9 +3,9 @@
 const planLabels = {
     oss: "OSS",
     "cloud:team": "Team",
+    "cloud:demo": "Demo",
     "self-hosted:enterprise": "Enterprise (Self-Hosted)",
     "self-hosted:enterprise-unlimited": "Enterprise (Self-Hosted) Unlimited",
-    "self-hosted:enterprise-custom": "Enterprise (Self-Hosted) Custom",
 } as const;
 export type Plan = keyof typeof planLabels;
 
@@ -21,14 +21,11 @@ const entitlements = [
 ] as const;
 export type Entitlement = (typeof entitlements)[number];
 
-export const isValidEntitlement = (entitlement: string): entitlement is Entitlement => {
-    return entitlements.includes(entitlement as Entitlement);
-}
-
 export const entitlementsByPlan: Record<Plan, Entitlement[]> = {
     oss: [],
     "cloud:team": ["billing", "multi-tenancy", "sso", "code-nav"],
     "self-hosted:enterprise": ["search-contexts", "sso", "code-nav"],
     "self-hosted:enterprise-unlimited": ["search-contexts", "public-access", "sso", "code-nav"],
-    "self-hosted:enterprise-custom": [],
+    // Special entitlement for https://demo.sourcebot.dev
+    "cloud:demo": ["public-access", "code-nav", "search-contexts"],
 } as const;
