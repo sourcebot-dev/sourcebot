337eedc633b163e9c059d97487e378c693a35361
msukkari <michael.sukkarieh@mail.mcgill.ca>
2025-05-31 15:24:50 -0700

    add news entry for structured logging

--- packages/web/src/lib/newsData.ts
+++ packages/web/src/lib/newsData.ts
@@ -1,22 +1,28 @@
 import { NewsItem } from "./types";
 
 export const newsData: NewsItem[] = [
-  {
-    unique_id: "code-nav",
-    header: "Code navigation",
-    sub_header: "Built in go-to definition and find references",
-    url: "https://docs.sourcebot.dev/docs/search/code-navigation"
-  },
-  {
-    unique_id: "sso",
-    header: "SSO",
-    sub_header: "We've added support for SSO providers",
-    url: "https://docs.sourcebot.dev/docs/configuration/authentication",
-  },
-  {
-    unique_id: "search-contexts",
-    header: "Search contexts",
-    sub_header: "Filter searches by groups of repos",
-    url: "https://docs.sourcebot.dev/docs/search/search-contexts"
-  }
+    {
+        unique_id: "structured-logging",
+        header: "Structured logging",
+        sub_header: "We've added support for structured logging",
+        url: "https://docs.sourcebot.dev/docs/more/structured-logging"
+    },
+    {
+        unique_id: "code-nav",
+        header: "Code navigation",
+        sub_header: "Built in go-to definition and find references",
+        url: "https://docs.sourcebot.dev/docs/search/code-navigation"
+    },
+    {
+        unique_id: "sso",
+        header: "SSO",
+        sub_header: "We've added support for SSO providers",
+        url: "https://docs.sourcebot.dev/docs/configuration/authentication",
+    },
+    {
+        unique_id: "search-contexts",
+        header: "Search contexts",
+        sub_header: "Filter searches by groups of repos",
+        url: "https://docs.sourcebot.dev/docs/search/search-contexts"
+    }
 ]; 
