c423be7a14ad89aace123236ab1dac2ecbe8301d
bkellam <bshizzle1234@gmail.com>
2025-06-17 13:56:00 -0700

    feedback

--- packages/shared/src/env.ts
+++ packages/shared/src/env.ts
@@ -7,18 +7,13 @@ export const env = createEnv({
         SOURCEBOT_EE_LICENSE_KEY: z.string().optional(),
         SOURCEBOT_PUBLIC_KEY_PATH: z.string(),
     },
-    // @NOTE: Please make sure of the following:
-    // - Make sure you destructure all client variables in
-    //   the `runtimeEnv` block below.
-    // - Update the Dockerfile to pass these variables as build-args.
     client: {
         NEXT_PUBLIC_SOURCEBOT_CLOUD_ENVIRONMENT: z.enum(SOURCEBOT_CLOUD_ENVIRONMENT).optional(),
     },
     clientPrefix: "NEXT_PUBLIC_",
-    runtimeEnv: {
-        ...process.env,
-
-        // For Next.js >= 13.4.4, you need to manually destructure client variables:
+    runtimeEnvStrict: {
+        SOURCEBOT_EE_LICENSE_KEY: process.env.SOURCEBOT_EE_LICENSE_KEY,
+        SOURCEBOT_PUBLIC_KEY_PATH: process.env.SOURCEBOT_PUBLIC_KEY_PATH,
         NEXT_PUBLIC_SOURCEBOT_CLOUD_ENVIRONMENT: process.env.NEXT_PUBLIC_SOURCEBOT_CLOUD_ENVIRONMENT,
     },
     emptyStringAsUndefined: true,
