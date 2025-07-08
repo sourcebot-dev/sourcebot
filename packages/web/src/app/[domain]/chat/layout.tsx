af4ba4c3e0875cff6346e232f2fe84c823971a4d
bkellam <bshizzle1234@gmail.com>
2025-07-08 11:16:06 -0700

    Adds a navigation guard to chat that warns the user that leaving the page will lose chat history

--- packages/web/src/app/[domain]/chat/layout.tsx
+++ packages/web/src/app/[domain]/chat/layout.tsx
@@ -4,6 +4,7 @@ import { ResizablePanelGroup } from '@/components/ui/resizable';
 import { ChatSidePanel } from './components/chatSidePanel';
 import { TopBar } from '../components/topBar';
 import { ChatName } from './components/chatName';
+import { NavigationGuardProvider } from 'next-navigation-guard';
 
 interface LayoutProps {
     children: React.ReactNode;
@@ -14,21 +15,25 @@ interface LayoutProps {
 
 export default function Layout({ children, params: { domain } }: LayoutProps) {
     return (
-        <div className="flex flex-col h-screen w-screen">
-            <TopBar
-                domain={domain}
-            >
-                <ChatName />
-            </TopBar>
-            <ResizablePanelGroup
-                direction="horizontal"
-            >
-                <ChatSidePanel
-                    order={1}
-                />
-                <AnimatedResizableHandle />
-                {children}
-            </ResizablePanelGroup>
-        </div>
+        // @note: we use a navigation guard here since we don't support resuming streams yet.
+        // @see: https://ai-sdk.dev/docs/ai-sdk-ui/chatbot-message-persistence#resuming-ongoing-streams
+        <NavigationGuardProvider>
+            <div className="flex flex-col h-screen w-screen">
+                <TopBar
+                    domain={domain}
+                >
+                    <ChatName />
+                </TopBar>
+                <ResizablePanelGroup
+                    direction="horizontal"
+                >
+                    <ChatSidePanel
+                        order={1}
+                    />
+                    <AnimatedResizableHandle />
+                    {children}
+                </ResizablePanelGroup>
+            </div>
+        </NavigationGuardProvider>
     )
 }
