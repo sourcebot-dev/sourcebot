374edddb65ece45e3c3cd34dcbb67f27e82139b9
bkellam <bshizzle1234@gmail.com>
2025-07-15 08:18:46 -0700

    wip on refactoring homepage

---
+++ packages/web/src/app/[domain]/components/homepage/agenticSearchInfo.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const AgenticSearchInfo = () => {
+    return (
+        <div className="flex flex-col items-center w-fit gap-6">
+            <p>todo</p>
+        </div>
+    )
+}
