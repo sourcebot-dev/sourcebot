aebd8df193c594c678ab8e9163b83930cebe45eb
Michael Sukkarieh <michael.sukkarieh@mail.mcgill.ca>
2025-07-27 10:11:58 -0700

    feat(search_contexts): Add ability to include/exclude connections in search contexts (#399)
    * implement connection filter for search contexts
    
    * changelog

--- packages/schemas/src/v3/index.type.ts
+++ packages/schemas/src/v3/index.type.ts
@@ -114,11 +114,19 @@ export interface SearchContext {
   /**
    * List of repositories to include in the search context. Expected to be formatted as a URL without any leading http(s):// prefix (e.g., 'github.com/sourcebot-dev/sourcebot'). Glob patterns are supported.
    */
-  include: string[];
+  include?: string[];
+  /**
+   * List of connections to include in the search context.
+   */
+  includeConnections?: string[];
   /**
    * List of repositories to exclude from the search context. Expected to be formatted as a URL without any leading http(s):// prefix (e.g., 'github.com/sourcebot-dev/sourcebot'). Glob patterns are supported.
    */
   exclude?: string[];
+  /**
+   * List of connections to exclude from the search context.
+   */
+  excludeConnections?: string[];
   /**
    * Optional description of the search context that surfaces in the UI.
    */
