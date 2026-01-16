9ef0f34d8100b47696278ff49794260254a43594
Brendan Kellam <bshizzle1234@gmail.com>
2026-01-15 18:54:14 -0800

    fix(web): Fix "Invalid line number XXX in 21-line document" (#745)

--- packages/web/src/app/[domain]/browse/[...path]/components/rangeHighlightingExtension.ts
+++ packages/web/src/app/[domain]/browse/[...path]/components/rangeHighlightingExtension.ts
@@ -16,6 +16,11 @@ export const rangeHighlightingExtension = (range: BrowseHighlightRange) => State
     create(state) {
         const { start, end } = range;
 
+        if (start.lineNumber > state.doc.lines || end.lineNumber > state.doc.lines) {
+            console.warn(`Highlight range is out of bounds: start.lineNumber=${start.lineNumber}, end.lineNumber=${end.lineNumber}, doc.lines=${state.doc.lines}`);
+            return Decoration.none;
+        }
+
         if ('column' in start && 'column' in end) {
             const from = state.doc.line(start.lineNumber).from + start.column - 1;
             const to = state.doc.line(end.lineNumber).from + end.column - 1;
