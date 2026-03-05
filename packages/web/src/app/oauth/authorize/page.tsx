2c3e9dc71164126d8fdc3cf98954f38b36e98cad
Brendan Kellam <brendan@sourcebot.dev>
2026-03-04 19:39:30 -0800

    fix(web): Fix issue where the /oauth/authorize page was always showing 'Authorization Error' because the page was being statically rendered.

--- packages/web/src/app/oauth/authorize/page.tsx
+++ packages/web/src/app/oauth/authorize/page.tsx
@@ -5,6 +5,8 @@ import { prisma } from '@/prisma';
 import { hasEntitlement } from '@sourcebot/shared';
 import { redirect } from 'next/navigation';
 
+export const dynamic = 'force-dynamic';
+
 interface AuthorizePageProps {
     searchParams: Promise<{
         client_id?: string;
