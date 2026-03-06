37f3bbe6e8b3baf5c383293f802880cd17118dc4
Brendan Kellam <brendan@sourcebot.dev>
2026-03-06 14:33:43 -0800

    nit

--- packages/web/src/app/api/(server)/[...slug]/route.ts
+++ packages/web/src/app/api/(server)/[...slug]/route.ts
@@ -1,16 +1,13 @@
 import { ErrorCode } from "@/lib/errorCodes"
-import { ServiceError } from "@/lib/serviceError"
+import { serviceErrorResponse } from "@/lib/serviceError"
 import { StatusCodes } from "http-status-codes"
-import { NextResponse } from "next/server"
 
-// Repeat for other methods or use a handler:
-const handler = () => NextResponse.json(
-    {
+const handler = () => {
+    return serviceErrorResponse({
         statusCode: StatusCodes.NOT_FOUND,
         errorCode: ErrorCode.NOT_FOUND,
         message: "This API endpoint does not exist",
-    } satisfies ServiceError,
-    { status: 404 }
-  )
-  
-  export { handler as GET, handler as POST, handler as PUT, handler as PATCH, handler as DELETE }
+    });
+}
+
+export { handler as GET, handler as POST, handler as PUT, handler as PATCH, handler as DELETE }
