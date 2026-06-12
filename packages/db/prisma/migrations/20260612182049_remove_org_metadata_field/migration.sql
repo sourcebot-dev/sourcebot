75c731a25efe9b01b93d923d22aea32d8df7b35a
Brendan Kellam <brendan@sourcebot.dev>
2026-06-12 11:22:57 -0700

    drop Org metadata field

---
+++ packages/db/prisma/migrations/20260612182049_remove_org_metadata_field/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `metadata` on the `Org` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Org" DROP COLUMN "metadata";
