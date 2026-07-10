ca52435e27f00aea4f4c9be7bbf51d75453fe16a
Brendan Kellam <brendan@sourcebot.dev>
2026-07-09 19:29:31 -0700

    s

--- packages/web/next.config.mjs
+++ packages/web/next.config.mjs
@@ -147,7 +147,7 @@ export default withSentryConfig(nextConfig, {
     org: process.env.SENTRY_ORG,
     project: process.env.SENTRY_WEBAPP_PROJECT,
     authToken: process.env.SENTRY_AUTH_TOKEN,
-    release: process.env.SENTRY_RELEASE,
+    release: { name: process.env.SENTRY_RELEASE },
 
     // Only print logs for uploading source maps in CI
     silent: !process.env.CI,
